ec96fc8deede205ebca5831779fdf4250baab80b
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2026-04-09 18:04:23 +0200

    discriminate between grabbing/using hitboxes of kitchen tools

--- godot/root/scenes/scene/game_scene/game_content/kichen/tools/kitchen_tool.gd
+++ godot/root/scenes/scene/game_scene/game_content/kichen/tools/kitchen_tool.gd
@@ -9,25 +9,28 @@ const RESET_TWEEN_DURATION := 0.3
 @export_group("Sauce")
 @export_custom(Registry.PROPERTY_HINT_CUSTOM, "uid://cgvbeut67x3ce") var sauce_ingredient: StringName
 
-
-
 var kitchen: Kitchen
 var storage_position: Vector2
+
+@onready var grabber_area_2d: Area2D = %GrabberArea2D
+
 
 func _ready() -> void:
 	storage_position = position
 	kitchen = get_node(kitchen_path)
+	grabber_area_2d.input_event.connect(_on_grabber_area_2d_input_event)
 
 
 func reset_tool() -> void:
 	create_tween().tween_property(self, "position", storage_position, RESET_TWEEN_DURATION).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 	create_tween().tween_property(self, "rotation", storage_rotation, RESET_TWEEN_DURATION)
 
+
 func _is_mouse_left_click(event: InputEvent) -> bool:
 	return event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()
 
 
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_grabber_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if _is_mouse_left_click(event) and kitchen:
 		var grabbed := kitchen.try_grab(self)
 		if grabbed:
